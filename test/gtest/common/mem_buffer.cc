e45447db292ac531b311b46ea8712123d02b6144
Nilesh M Negi <Nilesh.Negi@amd.com>
2023-10-19 17:42:36 -0500

    GTEST/COMMON: Add HIP_VERSION condition for ROCm compatibility (#28)
    Signed-off-by: nileshnegi <Nilesh.Negi@amd.com>

--- test/gtest/common/mem_buffer.cc
+++ test/gtest/common/mem_buffer.cc
@@ -33,7 +33,8 @@
 #endif
 
 #if HAVE_ROCM
-#  include <hip_runtime.h>
+#include <hip_runtime.h>
+#include <hip_version.h>
 
 #define ROCM_CALL(_code) \
     do { \
@@ -91,7 +92,13 @@ bool mem_buffer::is_rocm_managed_supported()
     }
 
     hipFree(dptr);
+
+#if HIP_VERSION >= 50500000
+    return attr.type == hipMemoryTypeUnified;
+#else
     return attr.memoryType == hipMemoryTypeUnified;
+#endif
+
 #else
     return false;
 #endif
